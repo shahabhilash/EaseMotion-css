43f98ca4d77df31b7f75e5287bfc525d12f6484a
shauryaparth1902-blip <shauryaparth1902@gmail.com>
2026-06-19 22:06:42 +0530

    fix: move phase 225 implementation to UI submission per CONTRIBUTING.md

--- server/routes/omegaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const evaluateController = require('../controllers/evaluateController');
-
-const router = express.Router();
-
-// POST /api/v1/easemotionomegaplatform/evaluate
-router.post('/easemotionomegaplatform/evaluate', evaluateController.evaluateFrontend);
-
-module.exports = router;
